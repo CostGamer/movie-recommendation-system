9a54d536d787f96ee44407a77de788f8c1fb1467
Vladimir <pdorou@bk.ru>
2025-01-04 15:27:36 +0800

    start of auth

---
+++ auth_service/app/repo/auth_repo.py
@@ -0,0 +1,12 @@
+# class AuthRepo:
+#     def __init__(self):
+#         pass
+
+#     async def encode_jwt(
+#             self,
+#             payload: dict,
+#             private_key: str = all_settings.private_key_path.read_text(),
+#             algorithm: str = all_settings.algorithm,
+#             expire_minutes: int = all_settings.access_token_expire_minutes,
+#             expire_timedelta: timedelta | None = None,
+#             ) ->:
